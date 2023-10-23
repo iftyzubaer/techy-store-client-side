c64d92aeab5ebdec07f00506b644529766b22835
iftyzubaer <iftyzubaer1304.com>
2023-10-23 17:18:51 +0600

    no element found page

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -20,9 +20,9 @@ const Products = () => {
     }, [name, products])
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-6 md:px-48 lg:px-60">
+        <div>
             {
-                product.map(product => <ProductsCard key={name} product={product}></ProductsCard>)
+                product.length === 0 ? <p className="text-center justify-center my-52 items-center font-semibold text-7xl">No Element Found</p> : <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-6 md:px-48 lg:px-60">{product.map(product => <ProductsCard key={name} product={product}></ProductsCard>)}</div>
             }
         </div>
     );
