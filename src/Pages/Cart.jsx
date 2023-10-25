574e9d17ce893f2b7718295c080a5cc4c0e01fe2
iftyzubaer <iftyzubaer1304.com>
2023-10-25 17:52:22 +0600

    view all cart products

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,30 +1,22 @@
 import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
-import { toast } from "react-toastify";
+import { useLoaderData } from "react-router-dom";
 
 const Cart = () => {
+
+    const cartItems = useLoaderData()
 
     const [cart, setCart] = useState([])
     const [noFound, setNoFound] = useState("")
 
     useEffect(() => {
-        const cartItems = JSON.parse(localStorage.getItem('cart'))
-
         if (cartItems) {
             setCart(cartItems)
         }
         else {
             setNoFound('No Data Found!')
         }
-    }, [])
-
-    const removeFromCart = id => {
-        const cartItems = JSON.parse(localStorage.getItem('cart'))
-        const remaining = cartItems.filter(idx => idx !== id)
-        console.log(cartItems, id, remaining);
-        localStorage.setItem('cart', JSON.stringify(remaining))
-        toast("Removed From Cart")
-    }
+    }, [cartItems])
 
     return (
         <div>
@@ -32,7 +24,7 @@ const Cart = () => {
                 noFound ? <p className="justify-center items-center flex h-[80vh]">{noFound}</p> :
                     <div className="mt-20 grid grid-cols-1 lg:grid-cols-2 gap-6">
                         {
-                            cart.map(product => <ProductList key={product._id} removeFromCart={removeFromCart} product={product}></ProductList>)
+                            cart.map(product => <ProductList key={product._id} product={product}></ProductList>)
                         }
                     </div>
             }
